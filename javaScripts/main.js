091045e7ee05a90bef3c3d825ab9358e78c4d013
Richard Yang <rich.yang88@gmail.com>
2019-08-07 11:55:28 -0400

    can now address invidual divs in rows

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -8,7 +8,7 @@ layout();
 // create divs for rows
 for(let i=0; i<6; i++){
 let newDivs = document.createElement('div');
-newDivs.className="row1sub ";
+newDivs.className="row1sub r1"+[i];
 document.querySelector('.row1').appendChild(newDivs);
 // document.querySelector('.row2').appendChild(newDivs);
 // document.querySelector('.row3').appendChild(newDivs);
