7923a89216f2441d2ebc2b16134de47bd59d26b2
Richard Yang <rich.yang88@gmail.com>
2019-08-07 11:40:23 -0400

    can gen row 1 now

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -8,7 +8,7 @@ layout();
 // create divs for rows
 for(let i=0; i<6; i++){
 let newDivs = document.createElement('div');
-newDivs.className="rows";
+newDivs.className="row1sub ";
 document.querySelector('.row1').appendChild(newDivs);
 // document.querySelector('.row2').appendChild(newDivs);
 // document.querySelector('.row3').appendChild(newDivs);
