8476fe3b32cc81cd91982119ab909a13c20a5d2f
Richard Yang <rich.yang88@gmail.com>
2019-08-08 15:38:36 -0400

    switching to keyframes again

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -12,30 +12,7 @@ function move(event){
     let questBox = document.getElementById('questionBg');
     questBox.style.display ="block";
     console.log("working")
-    //move center to out of screen
-    questBox.animate([
-        {transform: 'translateX(0px)'},
-        {transform: 'translateX(-1500px)'}
-    ],
-        {duration: 5000})
-
-    // pause
-    questBox.animate([
-        {transform: 'translateX(0px)'},
-        {transform: 'translateX(0px)'}
-    ],
-        {duration: 4000})
-
-    //move off screen to center
-    questBox.animate([
-        {transform: 'translateX(-1000px)'},
-        {transform: 'translateX(0px)'}
-        // PAUSE HERE
-        ],
-        {duration: 1000})
-    
-
-
+    questBox.classList.add('questAnimate')
 
 }
 
