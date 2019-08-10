7d8bbb172811e09888b84453acc3f1a6a8e685c4
Richard Yang <rich.yang88@gmail.com>
2019-08-10 11:46:36 -0400

    adding second btn function

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -46,8 +46,10 @@ function move(event){
         function check(event){
             if(event.target.id=='option1'){
                 console.log('Right');
-
-
+                
+                // $('#questionBg').removeClass('questAnimate');
+                question2();
+                $('#questionBg').removeClass('questAnimate');
             }else{
                 console.log('wrong');
                 return;
@@ -66,6 +68,69 @@ function move(event){
 document.querySelector('#start100Btn').addEventListener('click', move)
 document.querySelector('#start200Btn').addEventListener('click', move)
 document.querySelector('#start300Btn').addEventListener('click', move)
+
+
+//Buttons to start the row, making questions appear
+function question2(){
+    // $('#questionBg').removeClass('questAnimate');
+    let questBox = document.getElementById('questionBg');
+    // let rando= Math.floor(Math.random()*3);
+    console.log("Next Question working");
+    //questAnimate = class who's only purpose is to animate div attach to
+    //can be found first on CSS/style.css line 119
+    questBox.classList.add('questAnimate');
+
+    // Add hide class to buttons, seen on style.css line 69
+
+
+    //create div to add to #questionBg
+    let question = document.createElement("div");
+    //top row found on style.css near line 128
+    question.className='questionTopRow';
+    let questionShow= question.innerHTML = questionDb.row1BadgerQ[0].show1Q;
+    questBox.appendChild(question);
+
+    //for loop to pull from Questions Database
+    for(let i=0; i<questionDb.row1BadgerQ[0].show2A.length; i++){
+        let choice1=document.createElement('button');
+        choice1.className = "choiceBtn option"+[i];
+        choice1.id = "option"+[i];
+        console.log(choice1.id);
+        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1BadgerQ[0].show2A[i];
+        questBox.appendChild(choice1);
+
+        //event listener for choices 
+        choice1.addEventListener('click', check)
+
+        function check(event){
+            if(event.target.id=='option1'){
+                console.log('Right');
+                question2();
+
+
+            }else{
+                console.log('wrong');
+                return;
+            }
+        }
+        
+    }
+
+    //after 17 seconds remove class using Jquery
+    setTimeout(function(){
+        $('#questionBg').removeClass('questAnimate');
+        console.log("removing working")
+    },17000);
+}
+
+
+
+
+
+
+
+
+
 
 //Questions Obj
 let questionDb = {
