c76688b1564796ebb4c14f12422c87d17641a23c
Richard Yang <rich.yang88@gmail.com>
2019-08-10 20:46:45 -0400

    created answer div to be added and removed for correct answer

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -1,137 +1,3 @@
- 
-// function to create divs for rows // stretch goal
-// for(let i=0; i<6; i++){
-// let newDivs = document.createElement('div');
-// newDivs.className="row1sub r1"+[i];
-// document.querySelector('.row1').appendChild(newDivs);
-// // document.querySelector('.row2').appendChild(newDivs);
-// // document.querySelector('.row3').appendChild(newDivs);
-// }
-
-//Buttons to start the row, making questions appear
-function move(event){
-    let questBox = document.getElementById('questionBg');
-    let rando= Math.floor(Math.random()*3);
-    console.log("working");
-    //questAnimate = class who's only purpose is to animate div attach to
-    //can be found first on CSS/style.css line 119
-    questBox.classList.add('questAnimate');
-
-    // Add hide class to buttons, seen on style.css line 69
-    document.getElementById('start100Btn').classList.add('hideBtn');
-    document.getElementById('row1a1').innerHTML="You Started Here";
-    document.getElementById('row1a1').classList.add('row1b1');
-    document.getElementById('start200Btn').classList.add('hideBtn');
-    document.getElementById('start300Btn').classList.add('hideBtn');
-
-    //create div to add to #questionBg
-    let question = document.createElement("div");
-    //top row found on style.css near line 128
-    question.className='questionTopRow';
-    let questionShow= question.innerHTML = questionDb.row1hi1Q[rando].show1Q;
-    questBox.appendChild(question);
-
-    //for loop to pull from Questions Database
-    for(let i=0; i<questionDb.row1hi1Q[rando].show2A.length; i++){
-        let choice1=document.createElement('button');
-        choice1.className = "choiceBtn option"+[i];
-        choice1.id = "option"+[i];
-        console.log(choice1.id);
-        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1hi1Q[rando].show2A[i];
-        questBox.appendChild(choice1);
-
-        //event listener for choices 
-        choice1.addEventListener('click', check)
-
-        function check(event){
-            if(event.target.id=='option1'){
-                console.log('Right');
-                
-                // $('#questionBg').removeClass('questAnimate');
-                question2();
-                $('#questionBg').removeClass('questAnimate');
-            }else{
-                console.log('wrong');
-                return;
-            }
-        }
-        
-    }
-
-    //after 17 seconds remove class using Jquery
-    setTimeout(function(){
-        $('#questionBg').removeClass('questAnimate');
-        console.log("removing working")
-    },17000);
-}
-
-document.querySelector('#start100Btn').addEventListener('click', move)
-document.querySelector('#start200Btn').addEventListener('click', move)
-document.querySelector('#start300Btn').addEventListener('click', move)
-
-
-//Buttons to start the row, making questions appear
-function question2(){
-    // $('#questionBg').removeClass('questAnimate');
-    let questBox = document.getElementById('questionBg');
-    // let rando= Math.floor(Math.random()*3);
-    console.log("Next Question working");
-    //questAnimate = class who's only purpose is to animate div attach to
-    //can be found first on CSS/style.css line 119
-    questBox.classList.add('questAnimate');
-
-    // Add hide class to buttons, seen on style.css line 69
-
-
-    //create div to add to #questionBg
-    let question = document.createElement("div");
-    //top row found on style.css near line 128
-    question.className='questionTopRow';
-    let questionShow= question.innerHTML = questionDb.row1BadgerQ[0].show1Q;
-    questBox.appendChild(question);
-
-    //for loop to pull from Questions Database
-    for(let i=0; i<questionDb.row1BadgerQ[0].show2A.length; i++){
-        let choice1=document.createElement('button');
-        choice1.className = "choiceBtn option"+[i];
-        choice1.id = "option"+[i];
-        console.log(choice1.id);
-        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1BadgerQ[0].show2A[i];
-        questBox.appendChild(choice1);
-
-        //event listener for choices 
-        choice1.addEventListener('click', check)
-
-        function check(event){
-            if(event.target.id=='option1'){
-                console.log('Right');
-                question2();
-
-
-            }else{
-                console.log('wrong');
-                return;
-            }
-        }
-        
-    }
-
-    //after 17 seconds remove class using Jquery
-    setTimeout(function(){
-        $('#questionBg').removeClass('questAnimate');
-        console.log("removing working")
-    },17000);
-}
-
-
-
-
-
-
-
-
-
-
 //Questions Obj
 let questionDb = {
     //row 1, column hello questions
@@ -175,6 +41,156 @@ let questionDb = {
             }],//end of r1 Mushroom
 
 }
+
+
+
+// Buttons to start the row, making questions appear
+function move(event){
+    let questBox = document.getElementById('questionBg');
+    let answerBg = document.createElement('div');
+    answerBg.id="answerBgId";
+    
+    let rando= Math.floor(Math.random()*3);
+    console.log("working");
+    //questAnimate = class who's only purpose is to animate div attach to
+    //can be found first on CSS/style.css line 119
+    questBox.classList.add('questAnimate');
+
+    // Add hide class to buttons, seen on style.css line 69
+    document.getElementById('start100Btn').classList.add('hideBtn');
+    document.getElementById('row1a1').innerHTML="You Started Here";
+    document.getElementById('row1a1').classList.add('row1b1');
+    document.getElementById('start200Btn').classList.add('hideBtn');
+    document.getElementById('start300Btn').classList.add('hideBtn');
+
+    //create div to add to #questionBg
+    let question = document.createElement("div");
+    //top row found on style.css near line 128
+    question.className='questionTopRow';
+    let questionShow= question.innerHTML = questionDb.row1hi1Q[rando].show1Q;
+    questBox.appendChild(question);
+
+    //append answerBg to quetionBg div on index.html line 86
+    questBox.appendChild(answerBg);
+
+    //for loop to pull from Questions Database
+    for(let i=0; i<questionDb.row1hi1Q[rando].show2A.length; i++){
+        let choice1=document.createElement('button');
+        choice1.className = "choiceBtn option"+[i];
+        choice1.id = "option"+[i];
+        console.log(choice1.id);
+        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1hi1Q[rando].show2A[i];
+        answerBg.appendChild(choice1);
+
+        //event listener for choices 
+        choice1.addEventListener('click', check)
+
+        function check(event){
+            if(event.target.id=='option1'){
+                console.log('Right');
+                questBox.removeChild(question);
+                questBox.removeChild(answerBg);
+                
+                // choice1.removeChild('button');
+                question2();
+                // $('#questionBg').removeClass('questAnimate');
+            }else{
+                console.log('wrong');
+                return;
+            }
+        }
+        
+    }
+
+    //after 17 seconds remove class using Jquery
+    setTimeout(function(){
+        $('#questionBg').removeClass('questAnimate');
+        console.log("removing working")
+    },17000);
+}
+
+document.querySelector('#start100Btn').addEventListener('click', move)
+document.querySelector('#start200Btn').addEventListener('click', move)
+document.querySelector('#start300Btn').addEventListener('click', move)
+
+
+// question2();
+
+// Buttons to start the row, making questions appear
+function question2(event){
+    let questBox = document.getElementById('questionBg');
+    let answerBg = document.createElement('div');
+    answerBg.id="answerBgId";
+    
+    // let rando= Math.floor(Math.random()*3);
+    console.log("working");
+    //questAnimate = class who's only purpose is to animate div attach to
+    //can be found first on CSS/style.css line 119
+    questBox.classList.add('questAnimate');
+
+    // Add hide class to buttons, seen on style.css line 69
+    // document.getElementById('start100Btn').classList.add('hideBtn');
+    // document.getElementById('row1a1').innerHTML="You Started Here";
+    // document.getElementById('row1a1').classList.add('row1b1');
+    // document.getElementById('start200Btn').classList.add('hideBtn');
+    // document.getElementById('start300Btn').classList.add('hideBtn');
+
+    //create div to add to #questionBg
+    let question = document.createElement("div");
+    //top row found on style.css near line 128
+    question.className='questionTopRow';
+    let questionShow= question.innerHTML = questionDb.row1BadgerQ[0].show1Q;
+    questBox.appendChild(question);
+
+    //append answerBg to quetionBg div on index.html line 86
+    questBox.appendChild(answerBg);
+
+    //for loop to pull from Questions Database
+    for(let i=0; i<questionDb.row1BadgerQ[0].show2A.length; i++){
+        let choice1=document.createElement('button');
+        choice1.className = "choiceBtn option"+[i];
+        choice1.id = "option"+[i];
+        console.log(choice1.id);
+        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1BadgerQ[0].show2A[i];
+        answerBg.appendChild(choice1);
+
+        //event listener for choices 
+        choice1.addEventListener('click', check)
+
+        function check(event){
+            if(event.target.id=='option1'){
+                console.log('Right');
+                questBox.removeChild(question);
+                questBox.removeChild(answerBg);
+                
+                // choice1.removeChild('button');
+                question2();
+                // $('#questionBg').removeClass('questAnimate');
+            }else{
+                console.log('wrong');
+                return;
+            }
+        }
+        
+    }
+
+    //after 17 seconds remove class using Jquery
+    setTimeout(function(){
+        $('#questionBg').removeClass('questAnimate');
+        console.log("removing working")
+    },17000);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
