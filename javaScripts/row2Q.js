4770d9be4de2793e22c8ee11637e779eedf6974e
Richard Yang <rich.yang88@gmail.com>
2019-08-11 09:58:16 -0400

    row2 start working

--- javaScripts/row2Q.js
+++ javaScripts/row2Q.js
@@ -1,34 +1,41 @@
 // row 1 question 3();
 
 // Buttons to start the row, making questions appear
-function row1Q3(event){
+function row2Start(event){
     let questBox = document.getElementById('questionBg');
     let answerBg = document.createElement('div');
     answerBg.id="answerBgId";
     
-    // let rando= Math.floor(Math.random()*3);
+    let rando= Math.floor(Math.random()*3);
     console.log("working");
     //questAnimate = class who's only purpose is to animate div attach to
     //can be found first on CSS/style.css line 119
     questBox.classList.add('questAnimate');
 
+    // Add hide class to buttons, seen on style.css line 69
+    document.getElementById('start200Btn').classList.add('hideBtn');
+    document.getElementById('row2a1').innerHTML="You Started Here";
+    document.getElementById('row2a1').classList.add('row1b1');
+    document.getElementById('start100Btn').classList.add('hideBtn');
+    document.getElementById('start300Btn').classList.add('hideBtn');
+
     //create div to add to #questionBg
     let question = document.createElement("div");
     //top row found on style.css near line 128
     question.className='questionTopRow';
-    let questionShow= question.innerHTML = questionDb.row1MushQ[0].show1Q;
+    let questionShow= question.innerHTML = questionDb.row1hi1Q[rando].show1Q;
     questBox.appendChild(question);
 
     //append answerBg to quetionBg div on index.html line 86
     questBox.appendChild(answerBg);
 
     //for loop to pull from Questions Database
-    for(let i=0; i<questionDb.row1MushQ[0].show2A.length; i++){
+    for(let i=0; i<questionDb.row1hi1Q[rando].show2A.length; i++){
         let choice1=document.createElement('button');
         choice1.className = "choiceBtn option"+[i];
         choice1.id = "option"+[i];
         console.log(choice1.id);
-        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1MushQ[0].show2A[i];
+        let choiceBtn = choice1.innerHTML = (i+1) + ") " + questionDb.row1hi1Q[rando].show2A[i];
         answerBg.appendChild(choice1);
 
         //event listener for choices 
@@ -42,14 +49,13 @@ function row1Q3(event){
                 userScore+=100;
                 document.getElementById('userScoreSpan').innerText = userScore;
                 // choice1.removeChild('button');
-                row1Q3();
+                row1Q2();
                 // $('#questionBg').removeClass('questAnimate');
             }else{
                 console.log('wrong');
                 return;
             }
         }
-        
     }
 
     //after 17 seconds remove class using Jquery
@@ -59,4 +65,4 @@ function row1Q3(event){
     },17000);
 }
 
-document.querySelector('#start200Btn').addEventListener('click', row1Q3)
+document.querySelector('#start200Btn').addEventListener('click', row2Start)
